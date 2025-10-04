ec88fd5f5e52ad87ccaf4b92b4c935b47b2fb919
May-Bee-Lunar <maybeelunar0@gmail.com>
2025-10-04 15:54:45 -0700

    Merged ClimbPivotController into ClimbController
    :P

--- src/main/java/frc/robot/subsystems/climb/ClimbController.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbController.java
@@ -1,8 +1,13 @@
 package frc.robot.subsystems.climb;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.lib.generic_subsystems.superstructure.GenericSuperstructure.ControlMode;
+import frc.robot.subsystems.climb.climbPivot.ClimbPivot;
+import frc.robot.subsystems.climb.climbPivot.ClimbPivot.ClimbPivotTarget;
 import frc.robot.subsystems.climb.climbRollers.ClimbRollers;
 import org.littletonrobotics.junction.Logger;
 
@@ -11,15 +16,19 @@ public class ClimbController extends SubsystemBase {
   public enum ClimbState {
     IDLE,
     INTAKE,
-    HOLD
+    CLEAR,
+    CLIMB; // The actual action of climbing
   }
 
   private final ClimbRollers climbRollers;
 
+  private final ClimbPivot climbPivot;
+
   private ClimbState targetState = ClimbState.IDLE;
 
-  public ClimbController(ClimbRollers climbRollers) {
+  public ClimbController(ClimbRollers climbRollers, ClimbPivot climbPivot) {
     this.climbRollers = climbRollers;
+    this.climbPivot = climbPivot;
   }
 
   @Override
@@ -29,18 +38,71 @@ public class ClimbController extends SubsystemBase {
     switch (targetState) {
       case IDLE -> {
         climbRollers.setVoltageTarget(ClimbRollers.Target.IDLE);
+        climbPivot.setPositionTarget(ClimbPivot.ClimbPivotTarget.STOW);
       }
       case INTAKE -> {
         climbRollers.setVoltageTarget(ClimbRollers.Target.INTAKE);
+        climbPivot.setPositionTarget(ClimbPivot.ClimbPivotTarget.STOW);
       }
-      case HOLD -> {
+      case CLEAR -> {
         climbRollers.setVoltageTarget(ClimbRollers.Target.HOLD);
+        climbPivot.setPositionTarget(ClimbPivot.ClimbPivotTarget.CLEAR);
+      }
+      case CLIMB -> {
+        //need an if statement that checks if we are actually ready to climb
+        climbRollers.setVoltageTarget(ClimbRollers.Target.HOLD);
+        climbPivot.setPositionTarget(ClimbPivot.ClimbPivotTarget.TOP);
       }
     }
+    
+    climbPivot.periodic();
 
     climbRollers.periodic();
 
     Logger.recordOutput("Rollers/TargetState", targetState);
+  }
+
+  public Command setPositionTargetCommand(ClimbPivotTarget target) {
+    return new InstantCommand(
+        () -> {
+          climbPivot.setPositionTarget(target);
+        });
+  }
+
+   // Flick the climb to let coral fall out
+  public Command clearCoral() {
+    return new SequentialCommandGroup(
+        // Wait until we get to the clear position
+        new FunctionalCommand(
+            () -> {
+              climbPivot.setPositionTarget(ClimbPivotTarget.CLEAR);
+            },
+            () -> {},
+            (e) -> {},
+            climbPivot::reachedTarget),
+
+        // Then just go back up to stow
+        new InstantCommand(
+            () -> {
+              climbPivot.setPositionTarget(ClimbPivotTarget.STOW);
+            }));
+  }
+
+  public boolean climbPivotHitCage() {
+    return climbPivot.hitCage();
+  }
+
+  public ClimbPivotTarget getClimbPivotTarget() {
+    return climbPivot.getPositionTarget();
+  }
+
+  public void setPivotClimbTarget(ClimbPivotTarget target) {
+    climbPivot.setControlMode(ControlMode.POSITION);
+    climbPivot.setPositionTarget(target);
+  }
+
+  public void setStopped(boolean stopped) {
+    climbPivot.setControlMode(ControlMode.STOP);
   }
 
   public ClimbState getTargetState() {
