7869d36a945f3e21cb62d9e49407fb5c9bdbb1b7
Noland <nolan.bjorn.schuler@gmail.com>
2025-10-10 15:49:40 -0700

    Finished up the IntakeSensors Class but this thing still needs work
    Wrote the getters and the logic, but I still need to figure out how we can use the isReadyToIntake method so when it is true then the robot will actually intake the coral.

--- src/main/java/frc/robot/subsystems/intake/intake_sensors/IntakeSensors.java
+++ src/main/java/frc/robot/subsystems/intake/intake_sensors/IntakeSensors.java
@@ -1,11 +1,42 @@
 package frc.robot.subsystems.intake.intake_sensors;
 
+import org.littletonrobotics.junction.Logger;
+
 public class IntakeSensors {
 
   private IntakeSensorsIO intakeSensorsIO;
-  // creates separate autologged class for the values
-  // and logs all the inputs in advantage kit
+  private final int index;
   private IntakeSensorsIOInputsAutoLogged inputs = new IntakeSensorsIOInputsAutoLogged();
 
-  public IntakeSensors(IntakeSensorsIO intakeSensorsIO) {}
+  public IntakeSensors(IntakeSensorsIO intakeSensorsIO, int index) {
+    this.intakeSensorsIO = intakeSensorsIO;
+    this.index = index;
+  }
+
+  public void updateInputs() {
+    intakeSensorsIO.updateInputs(inputs);
+    Logger.processInputs("Intake/IntakeSensors" + index, inputs);
+    // FIXME: Change this directory so it's actually correct
+  }
+
+  // if the object is detected and within a certain distance then return true
+  public boolean isReadyToIntake() {
+    if (getIsDetected() == true
+        && getDistance() < 0.5 /*FIXME: CHANGE THIS VALUE TO WHAT WE NEED*/) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  // FIXME: make it so that if this method returns true, then the robot will align to or intake the
+  // piece
+
+  // getters
+  public double getDistance() {
+    return inputs.distance;
+  }
+
+  public boolean getIsDetected() {
+    return inputs.isDetected;
+  }
 }
