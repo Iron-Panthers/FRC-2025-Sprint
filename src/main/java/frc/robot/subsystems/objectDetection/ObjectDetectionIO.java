bbb03870887e2b08b44f33e69eb814d3131b9a1e
Bruce <brucebpeters12@gmail.com>
2025-11-17 16:33:24 -0800

    changed unit to degrees

--- src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionIO.java
+++ src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionIO.java
@@ -2,12 +2,15 @@ package frc.robot.subsystems.objectDetection;
 
 import org.littletonrobotics.junction.AutoLog;
 
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+
 public interface ObjectDetectionIO {
   @AutoLog
   public static class ObjectDetectionIOInputs {
     public boolean connected = false;
-    public double xErr = 0;
-    public double yErr = 0;
+    public Angle xErr = Units.Degrees.of(0);
+    public Angle yErr = Units.Degrees.of(0);
   }
 
   default void updateInputs(ObjectDetectionIOInputs inputs) {}
--- src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionIOLimelight.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.objectDetection;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.Units;
 
 public class ObjectDetectionIOLimelight implements ObjectDetectionIO {
   private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -9,7 +10,7 @@ public class ObjectDetectionIOLimelight implements ObjectDetectionIO {
   public ObjectDetectionIOLimelight() {}
 
   public void updateInputs(ObjectDetectionIOInputs inputs) {
-    inputs.xErr = table.getEntry("tx").getDouble(0);
-    inputs.yErr = table.getEntry("ty").getDouble(0);
+    inputs.xErr = Units.Degrees.of(table.getEntry("tx").getDouble(0));
+    inputs.yErr = Units.Degrees.of(table.getEntry("ty").getDouble(0));
   }
 }
