e628e649c9df6029802db24b585056281e293736
Bruce <brucebpeters12@gmail.com>
2025-11-20 16:28:43 -0800

    added constants and target rotation math to object detection

--- src/main/java/frc/robot/subsystems/objectDetection/ObjectDetection.java
+++ src/main/java/frc/robot/subsystems/objectDetection/ObjectDetection.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.objectDetection;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotState;
 import org.littletonrobotics.junction.Logger;
@@ -29,10 +31,34 @@ public class ObjectDetection extends SubsystemBase {
    * @return
    */
   public Rotation2d getTargetRotation() {
+    Angle targetPitch = inputs.yErr;
+    Angle targetYaw = inputs.xErr;
+
+    // t_y = c_z * tan(c_pitch + t_pitch) - c_y
+    double distanceY =
+        ObjectDetectionConstants.CAMERA_POSITION_CONSTANTS.z().in(Units.Meters)
+                * Math.tan(
+                    ObjectDetectionConstants.CAMERA_POSITION_CONSTANTS
+                        .pitchAngle()
+                        .plus(targetPitch)
+                        .in(Units.Radians))
+            - ObjectDetectionConstants.CAMERA_POSITION_CONSTANTS.y().in(Units.Meters);
+
+    // t_x = (c_y + distanceY) * tan(t_yaw) + c_x
+    double distanceX =
+        (ObjectDetectionConstants.CAMERA_POSITION_CONSTANTS.y().in(Units.Meters) + distanceY)
+                * Math.tan(targetYaw.in(Units.Radians))
+            + ObjectDetectionConstants.CAMERA_POSITION_CONSTANTS.x().in(Units.Meters);
+    
+    // arctan(t_x / t_y)
+    double angleToTarget = Math.atan2(distanceX, distanceY); // radians
+
+    Logger.recordOutput("ObjectDetection/Angle to Target", angleToTarget);
+
     return RobotState.getInstance()
         .getEstimatedPose()
         .getRotation()
-        .minus(new Rotation2d(inputs.xErr).plus(new Rotation2d(Math.PI)));
+        .minus(new Rotation2d(angleToTarget).plus(new Rotation2d(Math.PI)));
   }
   // Pass in a value so that the drive class rotates that many values; purpose is so that when a
   // button is pressed, the robot automatically aligns to the coral
--- src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionConstants.java
+++ src/main/java/frc/robot/subsystems/objectDetection/ObjectDetectionConstants.java
@@ -1,3 +1,30 @@
 package frc.robot.subsystems.objectDetection;
 
-public class ObjectDetectionConstants {}
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import frc.robot.Constants;
+
+public class ObjectDetectionConstants {
+
+  public static final CameraPositionConstants CAMERA_POSITION_CONSTANTS =
+      switch (Constants.getRobotType()) {
+        default -> new CameraPositionConstants(
+            Units.Meters.of(0.0), // x position
+            Units.Meters.of(0.0), // y position
+            Units.Meters.of(0.0), // z position
+            Units.Degrees.of(0.0), // pitch angle
+            Units.Degrees.of(0.0) // yaw angle
+            );
+      };
+
+  /**
+   * Camera position constants used for distance calculations
+   *
+   * @param x The x position of the camera from the center of the robot (left and right)
+   * @param y The y position of the camera from the center of the robot (forward and backward)
+   * @param z The z position of the camera from the center of the robot (up and down)
+   */
+  public static record CameraPositionConstants(
+      Distance x, Distance y, Distance z, Angle pitchAngle, Angle yawAngle) {}
+}
